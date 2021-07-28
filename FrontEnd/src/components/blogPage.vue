8d70131a6cb3849761911299716b74a2dbc2ab28
Redouane Bouabana <rredouane342@gmail.com>
2021-07-28 15:52:15 +0100

    comment setion

--- FrontEnd/src/components/blogPage.vue
+++ FrontEnd/src/components/blogPage.vue
@@ -31,60 +31,30 @@
             <div class="blog-article">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Accusamus voluptas quas libero maiores non aperiam ut velit
-              praesentium unde, mollitia atque minima debitis, et, optio
-              molestiae quod! Consectetur, dolore iste. Hic magnam consectetur,
-              minima nostrum, unde fuga, cumque quo illo numquam commodi culpa.
-              Rem, nulla expedita placeat dolor aut consequuntur. Lorem ipsum
-              dolor sit amet, consectetur adipisicing elit. Quidem corporis nemo
-              voluptas aliquid quod sequi delectus possimus ipsum voluptatibus
-              perspiciatis! Neque veritatis, provident architecto magni minima
-              reiciendis a labore esse. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Et nam quas deleniti obcaecati hic ipsum
-              cupiditate laudantium molestiae distinctio, consectetur tempore
-              rem voluptate. Adipisci aperiam commodi reprehenderit? Ipsam, odit
-              aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit. Accusamus voluptas quas libero maiores non aperiam ut velit
-              praesentium unde, mollitia atque minima debitis, et, optio
-              molestiae quod! Consectetur, dolore iste. Hic magnam consectetur,
-              minima nostrum, unde fuga, cumque quo illo numquam commodi culpa.
-              Rem, nulla expedita placeat dolor aut consequuntur. Lorem ipsum
-              dolor sit amet, consectetur adipisicing elit. Quidem corporis nemo
-              voluptas aliquid quod sequi delectus possimus ipsum voluptatibus
-              perspiciatis! Neque veritatis, provident architecto magni minima
-              reiciendis a labore esse. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Et nam quas deleniti obcaecati hic ipsum
-              cupiditate laudantium molestiae distinctio, consectetur tempore
-              rem voluptate. Adipisci aperiam commodi reprehenderit? Ipsam, odit
-              aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing
-              elit. Accusamus voluptas quas libero maiores non aperiam ut velit
-              praesentium unde, mollitia atque minima debitis, et, optio
-              molestiae quod! Consectetur, dolore iste. Hic magnam consectetur,
-              minima nostrum, unde fuga, cumque quo illo numquam commodi culpa.
-              Rem, nulla expedita placeat dolor aut consequuntur. Lorem ipsum
-              dolor sit amet, consectetur adipisicing elit. Quidem corporis nemo
-              voluptas aliquid quod sequi delectus possimus ipsum voluptatibus
-              perspiciatis! Neque veritatis, provident architecto magni minima
-              reiciendis a labore esse. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Et nam quas deleniti obcaecati hic ipsum
-              cupiditate laudantium molestiae distinctio, consectetur tempore
-              rem voluptate. Adipisci aperiam commodi reprehenderit? Ipsam, odit
-              aspernatur.
+        
             </div>
           </div>
         </div>
       </div>
+       <Scomment  />
     </div>
+     
   </div>
+
 </template>
 <script>
 import Like from "@/components/Misc/comment.vue";
 import Comment from "@/components/Misc/like.vue";
-
+import Scomment from "@/components/commentSection.vue";
 export default {
   components: {
     Like,
     Comment,
+    Scomment
   },
   setup() {},
 };
 </script>
+<style>
+
+</style>
